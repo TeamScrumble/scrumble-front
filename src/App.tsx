34ef1c0372d5b2975aa63470a2ce1b67414b88e4
wolfy916 <wolfy916@mobility42.io>
2025-05-16 22:02:13 +0900

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import LnbLayout from './layouts/LnbLayout'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import LnbLayout from "./layouts/LnbLayout";
 
 // 인가된 유저만 접근 허용 예시
 // function PrivateRoute({ children }: { children: JSX.Element }) {
@@ -14,12 +14,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LnbLayout />}>
-          <Route index element={<Home />}/>
+          <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
