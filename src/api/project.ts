0136ae0463cf103f20d62a88ef13340b28109036
unknown <power916@daum.net>
2025-05-25 18:30:29 +0900

    project 등록 성공후, 라우팅

--- src/api/project.ts
+++ src/api/project.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { API_BASE_URL, CommonResponse } from "./common";
+import { useNavigate } from "react-router-dom";
 
 export type Project = {
   rowid: number;
@@ -47,19 +48,21 @@ export type CreateProjectRequest = {
 
 export type CreateProjectResponse = CommonResponse & {
   data: {
-    rowid: number;
+    projectRowid: number;
   }
 };
 
 export const useCreateProject = () => {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
   return useMutation({  
     mutationFn: createProject,
-    onSuccess: () => {
+    onSuccess: ({ data }) => {
       // Create 성공이후, 리패칭
       queryClient.invalidateQueries({
         queryKey: ["projects"],
       });
+      navigate(`/project/${data.projectRowid}`);
     },
     onError: (error) => {
       console.error("요청 실패:", error);
